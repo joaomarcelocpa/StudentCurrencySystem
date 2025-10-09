014e6f5a64a2aee2774573cb8f333aec2b88b745
DevMiguelDiniz <miguelfdiniz@gmail.com>
2025-10-08 21:23:18 -0300

    criado ambiente do back-end

---
+++ código/backend/virtus/src/test/java/com/currencySystem/virtus/VirtusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currencySystem.virtus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
